612b39721cb65ac59cb6083ce3f1e126152f2fa7
sachin kumar jha <kjhasachin@gmail.com>
2022-01-12 15:58:10 +0545

    sachin push 3:58

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,14 +1,4 @@
-// loader 
-// $(document).ready(function(){
-// 	$('div#loading').removeAttr('id');
-// });
-var preloader = document.getElementById("loading");
-// window.addEventListener('load', function(){
-// 	preloader.style.display = 'none';
-// 	})
-function myFunction() {
-    preloader.style.display = 'none';
-};
+
 // Banner Slick Slider Starts
 $('.slider_banner').slick({
     infinite: true,
@@ -56,80 +46,48 @@ $('.slider_banner').slick({
     ]
 });
 // Banner Slick Slider Ends
-// Navigation Js Scroll Starts
-$(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        if (scroll > 50) {
-            $(".navigation-wrap").css("background", "white");
-            $(".nav-link").css("color", "#484d67");
-            $(".navigation-wrap").css("box-shadow", "rgb(0 0 0 / 20%) 0px 10px 10px -7px");
-            $(".navigation-wrap").css("padding", "10px 0px");
-            $(".navigation-wrap").css("border-bottom", "3px solid var(--main-color)");
-        } else {
-            $(".navigation-wrap").css("background", "white");
-            $(".nav-link").css("color", "#484d67");
-            $(".navigation-wrap").css("box-shadow", "none");
-            $(".navigation-wrap").css("padding", "15px 0px");
-            $(".navigation-wrap").css("border-bottom", "unset");
-        }
-    }) // Navigation Js Scroll Ends
-$(document).ready(function() {
-
-    // product Gallery and Zoom
-    // activation carousel plugin
-    var galleryThumbs = new Swiper('.gallery-thumbs', {
-        spaceBetween: 5,
-        freeMode: true,
-        watchSlidesVisibility: true,
-        watchSlidesProgress: true,
-        breakpoints: {
-            0: {
-                slidesPerView: 3,
-            },
-            992: {
-                slidesPerView: 4,
-            },
-        }
-    });
-    var galleryTop = new Swiper('.gallery-top', {
-        spaceBetween: 10,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+// CATEGORY SLIDER START 
+$(".slick_category").slick({
+    dots: false,
+    arrows: true,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    padding: 0,
+  
+    responsive: [
+      {
+        breakpoint: 1399,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 1,
         },
-        thumbs: {
-            swiper: galleryThumbs
+      },
+      {
+        breakpoint: 1080,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
         },
-    });
-    // change carousel item height
-    // gallery-top
-    let productCarouselTopWidth = $('.gallery-top').outerWidth();
-    $('.gallery-top').css('height', productCarouselTopWidth);
-    // gallery-thumbs
-    let productCarouselThumbsItemWith = $('.gallery-thumbs .swiper-slide').outerWidth();
-    $('.gallery-thumbs').css('height', productCarouselThumbsItemWith);
-    // activation zoom plugin
-    // var $easyzoom = $('.easyzoom').easyZoom();
-    // Dropdown Menu On Hover
+      },
+      {
+        breakpoint: 780,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 
 
-}); //   Video Play Button Pop Up Starts
-$('#play-video').on('click', function(e) {
-    e.preventDefault();
-    $('#video-overlay').addClass('open');
-    $("#video-overlay").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/ngElkyQ6Rhs" frameborder="0" allowfullscreen></iframe>');
-});
 
-$('.video-overlay, .video-overlay-close').on('click', function(e) {
-    e.preventDefault();
-    close_video();
-});
-
-$(document).keyup(function(e) {
-    if (e.keyCode === 27) { close_video(); }
-});
-
-function close_video() {
-    $('.video-overlay.open').removeClass('open').find('iframe').remove();
-};
-//   Video Play Button Pop Up Ends
+// CATEGORY SLIDER END  
